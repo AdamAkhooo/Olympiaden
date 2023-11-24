e996f0b597d91a1cd6dc59f18925ec8ef6c85b3a
AlemHalilovic <adamjamal0505@gmail.com>
2023-11-24 09:28:56 +0100

    Olympiaden uppgift 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv in ett ord som ska upprepas");
+         string ordet = Console.ReadLine();
+        
+        Console.WriteLine("hur många gånger ska ditt ord upprepas?");
+        int gånger = int.Parse(Console.ReadLine());
+
+        for(int i=0; i< gånger;i++)
+
+        
+
+        Console.WriteLine(ordet);
+
+    }
+}
