10f0a68ae60917407e8474f04acd2d327c40bfbb
AlemHalilovic <adamjamal0505@gmail.com>
2023-11-24 10:56:05 +0100

    uppg2

---
+++ uppg2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv N");
+        int N = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (int a = 1; a < N; a++)
+        {
+            int svar = a * (a+1) * (a+2);
+
+            if (svar < N)
+        
+
+            if (svar < N)
+            {
+            Console.WriteLine(svar);
+
+            }
+             else{
+                break;
+            }
+        }
+        
+        
+
+
+
+
+    }
+}
